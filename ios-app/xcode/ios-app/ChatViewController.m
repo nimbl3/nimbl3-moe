b9341f29c2fdedd8d39717f3012e51bf758e2f2f
Trung Le <trung@Trungs-MacBook-Pro.local>
2017-10-20 17:08:14 +0700

    Add iOS 2nd screen mapping

---
+++ ios-app/xcode/ios-app/ChatViewController.m
@@ -0,0 +1,39 @@
+//
+//  ChatViewController.m
+//  ios-app
+//
+//  Created by Trung Le on 10/20/17.
+//  Copyright © 2017 nimbl3. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@end
+
+@implementation ChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)submit:(id)sender {
+}
+@end
